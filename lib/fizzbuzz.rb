6242b6715bd40bab4e64d178e821f8f51edd34cc
zidehen <zoeidehen@gmail.com>
2021-11-10 18:50:39 +0000

    First test written

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,11 @@
+def fizzbuzz(number)
+  if number % 15 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0 
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
+  else
+    number  
+  end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,13 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
